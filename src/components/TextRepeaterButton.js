003db1eea5d6fb8f4216125d7bb975d3e747f33d
Saeed <saeed6433@gmail.com>
2023-06-23 15:30:19 -0400

    Modifying The State

--- src/components/TextRepeaterButton.js
+++ src/components/TextRepeaterButton.js
@@ -3,12 +3,16 @@ import {useState} from 'react'; //optional
   function TextRepeaterButton(){
     const [repetitions, setRepetitions] = useState(1);
 
+    const handleClick = () => {
+      setRepetitions(repetitions + 1);
+    }
+
     const textArray = [];
     for (let i = 0; i < repetitions; i++) {
-      textArray.push(<span key={i}>I like this text</span>);
+      textArray.push(<span key={i}>I like this text! </span>);
     }
     return (
-      <button className="TextRepeaterButton">
+      <button className="TextRepeaterButton" onClick={handleClick}>
         {textArray}
       </button>
     );
